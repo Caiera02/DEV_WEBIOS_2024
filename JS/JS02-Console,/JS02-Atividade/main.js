475f16f29e5caf6c333b59490721fd8db9dffc2a
Caiera02 <caiocezar2807@gmail.com>
2024-10-10 21:36:26 -0300

    Aulas de JS + Atividade

---
+++ JS/JS02-Console,/JS02-Atividade/main.js
@@ -0,0 +1,10 @@
+var nota = 10
+var nota2 = 5
+var nota3 = 3
+var nota4 = 5
+
+var soma = nota + nota2 + nota3 +nota4
+var result = soma / 4
+
+// alert( `A média do aluno com as 4 notas é: ${result}`)
+console.log(`A média do aluno com as 4 notas é: ${result}`)
---
+++ JS/JS02-Console,/main.js
@@ -0,0 +1,10 @@
+//let serve para usar numero
+
+//const é o ideal para usar numero
+
+// var nomeAluno = 'Caiera'
+// console.log(`Olá ${nomeAluno}`)
+
+var nomeAluno2= prompt( 'Digite seu nome: ' )
+
+console.log(`Olá ${nomeAluno2}` )
