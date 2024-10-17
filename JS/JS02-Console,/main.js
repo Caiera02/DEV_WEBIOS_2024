bf9058bfa2af34b84cd15c1985877b61d86ff945
Caiera02 <caiocezar2807@gmail.com>
2024-10-16 21:39:44 -0300

    atualização da aula 02

--- JS/JS02-Console,/main.js
+++ JS/JS02-Console,/main.js
@@ -5,6 +5,82 @@
 // var nomeAluno = 'Caiera'
 // console.log(`Olá ${nomeAluno}`)
 
-var nomeAluno2= prompt( 'Digite seu nome: ' )
+// var nomeAluno2= prompt( 'Digite seu nome: ' )
 
-console.log(`Olá ${nomeAluno2}` )
+// console.log(`Olá ${nomeAluno2}` )
+
+
+// -------------------------------------------------------------------------
+
+// let name = 'Ermeson';
+// console.log(name);
+
+// let nota1,nota2,media
+// nota1 = 5
+// nota2 = 10
+// media = (nota1 + nota2) / 2;
+// console.log(media)
+
+// + soma
+// - meno
+// / divisao
+
+
+// -----------------------------------------------------------
+// operadores relacionais
+// 'use strict'
+
+// let valor, nume1, nume2;
+
+// nume1 = 10
+// nume2 = 20
+// valor = nume1 > nume2 maior que
+// valor = nume1 >= nume2
+// valor = nume1 <= nume2 menor que 
+
+// valor = nume1 == nume2 isso aqui é pra saber se é igual 
+// valor = nume1 === nume2 isso aqui é pra saber se é igual e o mesmo tipo 
+
+// valor = nume1 != nume2 diferente
+
+// console.log(valor);
+
+
+// ================================================
+
+                    // Usando IF
+
+// let user, name, usuario, senha;
+
+// usuario = 'Caio'
+// senha = 123
+
+// user = prompt("Digite o seu usuario: ")
+// names = Number( prompt("Digite a senha: "))
+
+// if( user == usuario && names == senha){
+//     window.alert(" Deu certo essa merda !!")
+// }
+
+//-----------------------------------------------------
+// let nome = "Ermeson"
+// let num = 5;
+// console.log( typeof num)
+// console.log( nome.length)
+
+//-----------------------------------------------------
+
+            // Operador Ternario
+
+// let sol= true;
+// let dinheiro = 200;
+
+// let praia= sol == true && dinheiro >= 300 ? " Vamos a Praia" : "Não vamos para a praia."  // ||= ou
+// console.log(praia);
+
+let falta = 10
+
+let res= falta < 5 ?" Aprovado" : " Reprovado"
+console.log(res)
+
+
---
+++ JS/JS02-Console,/prompt.js
@@ -0,0 +1,16 @@
+// let nome = prompt('Qual é o seu nome caramba? ')
+
+// alert (" Olá Mundo !")
+
+// let name= prompt("Digite seu nome: ")
+
+// alert(`Boas-vinda ${name}`)
+
+let idade = Number(prompt("Digite sua idade: "))
+// let res = idade >= 18 ? " Você é de maior corno!!" : " Você é de menor, some daqui"
+
+if (idade >= 18) {
+    alert(" Você é de maior corno!!")
+} else {
+    alert(" Você é de menor, some daqui")
+}
