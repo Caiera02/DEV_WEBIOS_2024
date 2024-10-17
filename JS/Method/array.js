7a4b481a68316934ecb30e34fdf60a55e7392559
Caiera02 <caiocezar2807@gmail.com>
2024-10-17 20:52:31 -0300

    aula de hoje

---
+++ JS/Method/array.js
@@ -0,0 +1,6 @@
+// let grupo1 =['Baco', 'Gabi','Jeferson'] //array
+
+let car =[ 'Camaro', 'Mustang','ferrari','Marea', '208']
+console.log(car[3], car [4])
+// console.log(car[4])
+
---
+++ JS/Method/metodos.js
@@ -0,0 +1,25 @@
+// let cupons = 'DESC10' //10%
+
+// let menor = cupons.toLowerCase()
+// console.log(menor);
+
+// let maior = cupons.toUpperCase()
+// console.log(maior);
+
+// let IMC = 19.937889474
+// console.log( IMC.toFixed(3))
+
+//---------------------------------------------------------
+let nome = prompt("Digite seu nome: ")
+
+let idade = Number(prompt(" Qual é a sua idade ? "))
+
+let local = prompt(" Qual é o seu bairro ?")
+
+let maior= nome.toUpperCase()
+prompt(`${maior}`)
+
+let num1 = parseInt(prompt("Digite um numero: ")) 
+let num2 = parseInt(prompt("Digite o segundo numero: "))
+let res = alert(num1 + num2)
+alert(`Vacilão esse é o seu resultado ${res}`)
