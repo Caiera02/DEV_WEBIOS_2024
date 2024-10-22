6a19bc03550151d29078d206aacaf7e16b235238
Caiera02 <caiocezar2807@gmail.com>
2024-10-21 21:36:54 -0300

    aula JS04

---
+++ JS/JS04- Estruturas condicionais/script.js
@@ -0,0 +1,34 @@
+//Verificando se o Caio está com a blusa do IOS
+
+const blusa = true//prompt('Você esta com camiseta ?')
+
+if( blusa == true){
+    console.log(" Você pode assistir a aula ")
+}
+else{
+    console.log("Não vai entrar boco !!")
+    console.log("Até amanhã, com a sua blusa !!")
+}
+console.log('-----------------------     -------------------------')
+
+let pontos = 100
+
+if( pontos >= 1000){
+    console.log('Passou para a fase 2')
+}
+else(
+    console.log('Você não passou !!')
+)
+
+console.log('-----------------------     -------------------------')
+
+// resto da divisao do numero por 2 for 0 entao  é par
+
+let imparPar = 18
+
+if( imparPar %2 == 0){
+    console.log(" Par")
+}
+else{
+    console.log( 'Impar')
+}
