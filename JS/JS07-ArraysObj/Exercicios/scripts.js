86191897d110fda7b8700009e6f12a2bd93386e9
Caiera02 <caiocezar2807@gmail.com>
2024-11-04 15:42:30 -0300

    Exercicios aula 07

---
+++ JS/JS07-ArraysObj/Exercicios/scripts.js
@@ -0,0 +1,19 @@
+let aluno = [
+    { nome: 'Ana', idade: 17, nota: 8, ano: '2°b' },
+    { nome: 'Bruno', idade: 16, nota: 6, ano: '2°C' },
+    { nome: 'Veronica', idade: 16, nota: 9, ano: '2°C' },
+    { nome: "Marta", idade: 15, nota: 5, ano: "3°C" },
+    { nome: "Brenno", idade: 19, nota: 6, ano: "3°C" },
+    { nome: "Maria", idade: 14, nota: 4, ano: "1°F" }
+]
+
+// console.log(aluno)
+
+//console.log(aluno[1]);
+
+//console.log( `A nota da Maria é: ${aluno[5].nota}`);
+
+let convert = JSON.stringify(aluno)
+
+console.log(JSON.parse(convert));
+
