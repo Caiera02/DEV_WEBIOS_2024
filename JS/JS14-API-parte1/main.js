baa10a9b8f3e8ea202f07705b1670aa25ca33bbc
Caiera02 <caiocezar2807@gmail.com>
2024-11-19 21:31:20 -0300

    API

--- JS/JS14-API-parte1/main.js
+++ JS/JS14-API-parte1/main.js
@@ -1,21 +1,32 @@
 // Hoje falaremos de API
 
-let url= 'https://dog.ceo/api/breed/pitbull/images/random'
+let url = 'https://dog.ceo/api/breed/pitbull/images/random'
 
-const DogApi = async()=>{
+const DogApi = async () => {
     // Faço a consulta na API  e retorno o JSON na variavel resp
-            //aqui ele fala para esperar por isso usa o await
-            //fetch faz a consulta e validação
-    let resp = await fetch(url)
+    //aqui ele fala para esperar por isso usa o await
+    //fetch faz a consulta e validação
+    await fetch(url).then((response) =>{
+        return response.json();
 
-   
-    
 
-    
+    }).then((response)=>{
+        document.getElementById('mostraJSON').innerText= JSON.stringify(response)
+        let img =`
+        <img src="${response.message} " alt="">
+        `
+        document.getElementById('apresentaImg').innerHTML = img
+    })
+
+
+
+
+
     // Pegar o retorno JSON  e transforma ele em um Objeto
 
-    let exemplo = resp.json()
-    console.log(exemplo);
+    // Apresenta o link
+    // let exemplo = resp.json()
+    // console.log(exemplo);
     // https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg
 
 }
---
+++ JS/JS15- API/main.js
@@ -0,0 +1,46 @@
+let cep = document.getElementById('cep')
+
+let url = `https://viacep.com.br/ws/${cep.value}/json/`
+
+// async function BuscaCep() {
+//     let resp =await fetch(url).then((response) =>{
+//         // Transforma JSON em OBJ
+//         return response.json();
+
+//     }).then((response) => { 
+//         document.getElementById('cep').value = response.cep
+
+//         document.getElementById('lg').value = response.logradouro
+
+//         document.getElementById('comp').value = response.complemento
+//         document.getElementById('bairro').value = response.bairro
+        
+//     })
+
+//     // let exemplo = res.json();
+//     // console.log(exemplo)
+// }
+
+// BuscaCep();
+
+async function BuscaCep() {
+    await fetch(url).then((response) =>{
+        // Transforma JSON em OBJ
+        return response.json();
+
+    }).then((response) => { 
+        document.getElementById('cep').value = response.cep
+
+        document.getElementById('lg').value = response.logradouro
+
+        document.getElementById('comp').value = response.complemento
+        document.getElementById('bairro').value = response.bairro
+        
+    })
+
+    // let exemplo = res.json();
+    // console.log(exemplo)
+}
+
+// BuscaCep();
+
