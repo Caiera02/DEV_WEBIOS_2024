cf387495c5bbeef5832e466111fd54db88c674dc
Caiera02 <caiocezar2807@gmail.com>
2024-11-21 15:10:42 -0300

    Resolucao do codigo pra buscar a Api

--- JS/JS15- API/main.js
+++ JS/JS15- API/main.js
@@ -1,6 +1,7 @@
-let cep = document.getElementById('cep')
 
-let url = `https://viacep.com.br/ws/${cep.value}/json/`
+
+
+// let url = `https://viacep.com.br/ws/${cep}/json/`
 
 // async function BuscaCep() {
 //     let resp =await fetch(url).then((response) =>{
@@ -14,7 +15,7 @@ let url = `https://viacep.com.br/ws/${cep.value}/json/`
 
 //         document.getElementById('comp').value = response.complemento
 //         document.getElementById('bairro').value = response.bairro
-        
+
 //     })
 
 //     // let exemplo = res.json();
@@ -24,22 +25,25 @@ let url = `https://viacep.com.br/ws/${cep.value}/json/`
 // BuscaCep();
 
 async function BuscaCep() {
-    await fetch(url).then((response) =>{
+
+    let cep1= document.getElementById('cep').value
+    // let url = `https://viacep.com.br/ws/ ${cep1}/json/`
+    let url = 'https://viacep.com.br/ws/'+ cep1 +'/json/'
+
+    await fetch(url).then((response) => {
         // Transforma JSON em OBJ
         return response.json();
 
-    }).then((response) => { 
+    }).then((response) => {
         document.getElementById('cep').value = response.cep
 
         document.getElementById('lg').value = response.logradouro
 
         document.getElementById('comp').value = response.complemento
         document.getElementById('bairro').value = response.bairro
-        
-    })
+        document.getElementById('estado').value = response.estado
 
-    // let exemplo = res.json();
-    // console.log(exemplo)
+    })
 }
 
 // BuscaCep();
